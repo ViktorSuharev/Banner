ed764b587b6704314d458a82759f0cbb2014ada1
ViktorSuharev <viktor.suhareff@yandex.ru>
2018-04-14 14:02:07 +0300

    fix app name

--- src/main/java/com/visu/banner/BannerApplication.java
+++ src/main/java/com/visu/banner/BannerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HhApplication {
+public class BannerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HhApplication.class, args);
+		SpringApplication.run(BannerApplication.class, args);
 	}
 }
--- src/test/java/com/visu/banner/BannerApplicationTests.java
+++ src/test/java/com/visu/banner/BannerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HhApplicationTests {
+public class BannerApplicationTests {
 
 	@Test
 	public void contextLoads() {
